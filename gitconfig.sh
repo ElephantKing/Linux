1c5b4a6d8b49140ffd68bd54fe52ab0089c49632
elephantking <bupt_shufeng@163.com>
2017-09-23 15:53:17 +0800

    set git init file

---
+++ gitconfig.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global color.ui true
+git config --global alias.s-log "log --pretty=oneline"
+git config --global alias.f-log "log --pretty=fuller"
+git config --global alias.c-df "diff --cached"
+git config --global alias.h-df "diff HEAD"
+git config --global push.default simple
+git config --global user.name "ElephantKing"
+git config --global user.email "bupt_shufeng@163.com"
+
