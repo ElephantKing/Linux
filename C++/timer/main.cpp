67bbb3dee3aee860a099415e4e30ac86b928be35
ElephantKing <bupt_shufeng@163.com>
2017-09-26 21:17:01 +0800

    add get time function

--- C++/timer/main.cpp
+++ C++/timer/main.cpp
@@ -20,6 +20,18 @@ public:
 
 int main()
 {
+        {
+                //get time(seconds) distance from 1970.1.1:00:00:00
+                time_t now = time(NULL);
+                time_t now_;
+                time(&now_);
+                printf("now=%ld,now_=%ld\n",now, now_);
+
+                //get time(useconds,1/10^6s) distance from 1970.1.1:00:00:00
+                timeval now_tv;
+                gettimeofday(&now_tv, NULL);
+                printf("now_tv.sec=%ld, now_tv.usec=%ld\n",now_tv.tv_sec,now_tv.tv_usec);
+        }
         Task task;
         while(1)
         {
